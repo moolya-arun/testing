a1e7418b0456c126ebc2223c8bd91faecb4db2fe
moolya-arun <128695287+moolya-arun@users.noreply.github.com>
2023-03-27 19:16:58 +0530

    Add files via upload

---
+++ ConcatenateStrings.java
@@ -0,0 +1,11 @@
+package com.learning;
+
+public class ConcatenateStrings {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        String result = str1.concat(" " + str2);
+        System.out.println("Concatenated string is: " + result);
+    }
+}
+
